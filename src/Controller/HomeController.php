c8a3fdcc2fc0c77ab712ee368c07c63b61e2d498
ClaireV38 <claireverdier00@gmail.com>
2021-02-02 22:29:09 +0100

    display several users using github API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
@@ -13,8 +14,15 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(HttpClientInterface $client): Response
     {
-        return $this->render('home/index.html.twig');
+        $response = $client->request(
+            'GET',
+            'https://api.github.com/users'
+        );
+        $users = $response->toArray();
+        return $this->render('home/index.html.twig', [
+            'users' => $users,
+        ]);
     }
 }
