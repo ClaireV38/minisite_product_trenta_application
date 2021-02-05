1867bc706b7c5e6e478940904a03ebb97ee7b2e1
ClaireV38 <claireverdier00@gmail.com>
2021-02-05 22:56:56 +0100

    generate fixtures

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class ProductFixtures extends Fixture
+{
+    /**
+     * nb objects to create
+     * @var int
+     **/
+    public const NB_OBJECT = 60;
+
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $nbObjectByCategory = round(self::NB_OBJECT / count(categoryFIXTURES::CATEGORIES));
+
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i = 1; $i <= count(categoryFIXTURES::CATEGORIES); $i++) {
+            for ($j = 1; $j <= $nbObjectByCategory; $j++) {
+                $product = new Product();
+                $product->setTitle($faker->sentence($nbWords = 3, $variableNbWords = true));
+                $product->setCategory($this->getReference('category_' . $i));
+                $product->setPrice($faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 500));
+                $product->setPicture(categoryFIXTURES::CATEGORIES[$i - 1] . rand ( 1 , 10));
+                $product->setAvailableSize($faker->bothify('#?,#?,#?'));
+                $product->setAvailableColor($faker->sentence($nbWords = 4, $variableNbWords = true));
+                $product->setDescription($faker->text($maxNbChars = 200));
+                $product->setCreatedAt(new DateTime('now'));
+                $product->setUpdatedAt(new DateTime('now'));
+                $manager->persist($product);
+                $this->addReference('product_' . (($i - 1) * $nbObjectByCategory + $j), $product);
+            }
+        }
+        $manager->flush();
+    }
+}
